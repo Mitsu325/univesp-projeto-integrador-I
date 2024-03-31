fd66fa1f79c3830be055940818d7be506c3df00f
Mitsu325 <patricia.mashiba@gmail.com>
2024-03-31 20:09:02 -0300

    feat: create Manager Model

--- managers/models.py
+++ managers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Manager(models.Model):
+    USER_TYPES = (
+        ("recruitment_manager", "Recruitment Manager"),
+        ("department_manager", "Department Manager"),
+        ("team_lead", "Team Lead"),
+    )
+
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=80, unique=True)
+    password = models.CharField(max_length=50)
+    user_type = models.CharField(
+        max_length=50, choices=USER_TYPES, default="recruitment_manager"
+    )
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
--- resume_management/settings.py
+++ resume_management/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'managers.apps.ManagersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
