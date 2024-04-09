3117f344fd515e262d7f24497d026dab54b210e2
Mitsu325 <patricia.mashiba@gmail.com>
2024-04-08 21:33:00 -0300

    chore: change database engine

--- resume_management/settings.py
+++ resume_management/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "resume_management.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASSWORD"),
