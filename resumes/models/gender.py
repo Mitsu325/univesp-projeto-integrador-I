15e3b64ef9e6f9d7657a1bd2d94fc86176e285c8
Mitsu325 <patricia.mashiba@gmail.com>
2024-04-08 21:33:49 -0300

    feat: add key column in gender model

--- resumes/models/gender.py
+++ resumes/models/gender.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Gender(models.Model):
     name = models.CharField(max_length=50)
+    key = models.CharField(max_length=50, default="other")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
