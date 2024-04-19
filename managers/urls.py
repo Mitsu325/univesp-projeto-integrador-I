549c85b22b2db67194ad55c820f663ff445f65c2
johan <johan@Ubuntu.myguest.virtualbox.org>
2024-04-18 22:04:11 -0300

    managers-template-v1

--- managers/urls.py
+++ managers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from .views import index
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index.html", views.index, name="index"),
 ]
