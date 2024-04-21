a380f7bc51248e6eeeda5b68c475f76f8f3d0747
Mitsu325 <patricia.mashiba@gmail.com>
2024-04-21 18:45:36 -0300

    feat: add namespace in apps

--- managers/urls.py
+++ managers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
 from . import views
+
+app_name = "managers"
 
 urlpatterns = [
     path("", views.index, name="index"),
--- resumes/urls.py
+++ resumes/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "resumes"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("edit", views.edit, name="edit"),
