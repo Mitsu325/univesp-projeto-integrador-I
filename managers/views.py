4891c22ad6bfd23d2aa3877c066e5f517e53cf2d
Mitsu325 <patricia.mashiba@gmail.com>
2024-04-21 20:36:18 -0300

    feat: login manager

--- managers/views.py
+++ managers/views.py
@@ -1,6 +1,20 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import check_password
+from django.contrib import messages
+from .models import Manager
 
 
 def index(request):
-    return render(request, "managers/index.html", {})
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        try:
+            manager = Manager.objects.get(email=email)
+            if check_password(password, manager.password):
+                request.session["manager_id"] = manager.id
+                return redirect("management:index")
+            else:
+                messages.error(request, "E-mail ou senha incorretos.")
+        except Manager.DoesNotExist:
+            messages.error(request, "E-mail ou senha incorretos.")
+    return render(request, "managers/index.html")
