6918e2dc2856b216d7c83df4574d71b8ec168c9e
Mitsu325 <patricia.mashiba@gmail.com>
2024-04-21 21:17:51 -0300

    feat: create management base template

--- management/views.py
+++ management/views.py
@@ -1,9 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the management index.")
+    context = {
+        "menu_items": [
+            {"text": "Candidatos", "url": "/management/"},
+            {"text": "Configuração", "url": "/management/config"},
+        ],
+    }
+    return render(request, "management/index.html", context)
 
 
 def config(request):
-    return HttpResponse("Hello, world. You're at the management config.")
+    context = {
+        "menu_items": [
+            {"text": "Candidatos", "url": "/resumes/"},
+            {"text": "Configuração", "url": "/resumes/config"},
+        ],
+    }
+    if request.method == "POST":
+        if "logout" in request.POST:
+            request.session.flush()
+            return redirect("managers:index")
+    return render(request, "management/config.html", context)
